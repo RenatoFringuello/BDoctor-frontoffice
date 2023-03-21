145c02a212c2b65410a78dea701e2f06da3e6904
Claudio-Emmolo <claudioemmolo90@gmail.com>
2023-03-21 16:44:11 +0100

    Create scaffolding

---
+++ src/pages/AppHome.vue
@@ -0,0 +1,17 @@
+<script>
+import HelloWorld from '../components/HelloWorld.vue';
+
+export default {
+    name: 'AppHome',
+    components: { HelloWorld },
+}
+</script>
+
+<template>
+    <div>
+        Home
+        <HelloWorld />
+    </div>
+</template>
+
+<style lang="scss" scoped></style>
