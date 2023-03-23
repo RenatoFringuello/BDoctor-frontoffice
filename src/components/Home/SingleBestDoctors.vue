f3e086914d2be785f94aaedfa2dbe0297396a249
Claudio-Emmolo <claudioemmolo90@gmail.com>
2023-03-23 17:41:26 +0100

    Fix best doctors cards

--- src/components/Home/SingleBestDoctors.vue
+++ src/components/Home/SingleBestDoctors.vue
@@ -13,15 +13,19 @@ export default {
         </div>
         <div class="container">
             <!-- Sm -->
-            <div class="row scroll-row pb-2 d-flex d-lg-none flex-nowrap">
-                <div class="doctor-card mx-2" v-for="index in 8">
+            <div class="row scroll-row pb-2 d-flex d-xl-none flex-nowrap">
+                <div class="col-5" v-for="index in 8 ">
+                    <div class=" doctor-card">
 
+                    </div>
                 </div>
             </div>
             <!-- Lg -->
-            <div class="row scroll-row pb-2 d-none d-lg-flex justify-content-center">
-                <div class="doctor-card m-2 " v-for="index in 8">
+            <div class="row scroll-row g-3 pb-2 d-none d-xl-flex justify-content-center">
+                <div class="col-3" v-for="index in 8 ">
+                    <div class=" doctor-card">
 
+                    </div>
                 </div>
             </div>
         </div>
@@ -41,7 +45,7 @@ h3 {
 
 
 div.doctor-card {
-    width: 300px;
+    // width: 300px;
     height: 150px;
     border: 1px solid $main-doc-color;
 }
