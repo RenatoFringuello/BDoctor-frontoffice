955f98f117649e7b90f211bb0bf1f82d54551a30
Claudio-Emmolo <claudioemmolo90@gmail.com>
2023-03-27 16:46:01 +0200

    Print error message on message form

--- src/components/Doctors/DoctorsMessage.vue
+++ src/components/Doctors/DoctorsMessage.vue
@@ -20,13 +20,14 @@ export default {
     methods: {
         sendMessageForm() {
             const formData = {
-                user_id: this.route.params.id,
                 name: this.name,
                 lastname: this.lastname,
                 email: this.email,
-                content: this.content
+                content: this.content,
+                user_id: this.route.params.id,
             }
 
+            console.log(formData)
             axios.post('http://127.0.0.1:8000/api/message', formData)
                 .then((response) => {
                     this.success = response.data.success;
@@ -48,39 +49,44 @@ export default {
 </script>
 <template>
     <section id="form-message">
-        <form>
-            <div class="mb-4 row">
-                <div class="col-md-6">
-                    <label for="name" class="col-md-4">
-                        Name
-                    </label>
-                    <input v-model="name" id="name" type="text" class="form-control" name="telephone" autocomplete="name"
-                        required minlength="2" maxlength="255">
-                </div>
-                <div class="col-md-6">
-                    <label for="lastname" class="col-md-4">
-                        Lastname
-                    </label>
-                    <input v-model="lastname" id="lastname" type="text" class="form-control" name="lastname"
-                        autocomplete="lastname" required minlength="2" maxlength="255">
-                </div>
-                <div class="col-md-6">
-                    <label for="email" class="col-md-4" required minlength="2" maxlength="255">
-                        Email
-                    </label>
-                    <input v-model="email" id="email" type="email" class="form-control" name="email" autocomplete="email"
-                        autofocus minlength="9" required maxlength="255">
-                </div>
-                <div class="col-md-6">
-                    <label for="content" class="form-label">
-                        Content
-                    </label>
-                    <textarea v-model="content" class="form-control" name="content" id="content" rows="3" required
-                        minlength="10" maxlength="1000"></textarea>
-                </div>
+        <div class="mb-4 row">
+            <div class="col-md-6">
+                <label for="name" class="col-md-4">
+                    Name
+                </label>
+                <input v-model="name" id="name" type="text" class="form-control" name="telephone" autocomplete="name"
+                    :class="(this.errors.name) ? 'is-invalid' : ''">
+                <span v-if="this.errors.name" class="text-danger">{{ errors.name[0] }}</span>
             </div>
-            <button type="submit" class="btn btn-primary" @click="sendMessageForm()">Send Message</button>
-        </form>
+            <div class="col-md-6">
+                <label for="lastname" class="col-md-4">
+                    Lastname
+                </label>
+                <input v-model="lastname" id="lastname" type="text" class="form-control" name="lastname"
+                    autocomplete="lastname" :class="(this.errors.lastname) ? 'is-invalid' : ''">
+                <span v-if="this.errors.name" class="text-danger">{{ errors.lastname[0] }}</span>
+
+            </div>
+            <div class="col-md-6">
+                <label for="email" class="col-md-4">
+                    Email
+                </label>
+                <input v-model="email" id="email" type="email" class="form-control" name="email" autocomplete="email"
+                    autofocus :class="(this.errors.email) ? 'is-invalid' : ''">
+                <span v-if="this.errors.email" class="text-danger">{{ errors.email[0] }}</span>
+
+            </div>
+            <div class="col-md-6">
+                <label for="content" class="form-label">
+                    Content
+                </label>
+                <textarea v-model="content" class="form-control" name="content" id="content" rows="3"
+                    :class="(this.errors.content) ? 'is-invalid' : ''"></textarea>
+                <span v-if="this.errors.content" class="text-danger">{{ errors.content[0] }}</span>
+
+            </div>
+        </div>
+        <button type="submit" class="btn btn-primary" @click="sendMessageForm()">Send Message</button>
     </section>
 </template>
 <style lang="scss" scoped></style>
