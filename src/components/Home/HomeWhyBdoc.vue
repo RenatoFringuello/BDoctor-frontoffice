0661ec4169f70aeb85dd644ad0c7f087237b4fd6
RenatoFringuello <renatofringuello@hotmail.it>
2023-04-01 12:53:05 +0200

    remove extra row

--- src/components/Home/HomeWhyBdoc.vue
+++ src/components/Home/HomeWhyBdoc.vue
@@ -26,9 +26,8 @@ export default {
 <template>
     <section id="why-bdoc">
         <div class="container">
-            <div class="row py-5">
+            <div class="row gy-4 gy-lg-0 py-5">
                 <h2 class="section-title">Why BDoctor?</h2>
-                <div class="row gy-4 gy-lg-0">
                     <div class="col-12 col-lg-4" v-for="whyElement in whyList">
                         <div class="info-box d-flex justify-content-center align-items-center flex-column">
                             <h3 class="text-center mb-3">
@@ -39,8 +38,6 @@ export default {
                             </p>
                         </div>
                     </div>
-
-                </div>
             </div>
         </div>
     </section>
