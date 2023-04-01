e44351d6f2137cd708286ef7c8859703fc276f29
RenatoFringuello <renatofringuello@hotmail.it>
2023-04-01 12:16:46 +0200

    remove scroll small

--- src/components/Home/HomeTalkAboutUs.vue
+++ src/components/Home/HomeTalkAboutUs.vue
@@ -8,9 +8,8 @@ export default {
             store,
             newsList: ['1', '2', '8', '3', '4', '5', '6', '7', 'B1', 'B2', 'B3', 'B4', 'B5', 'B6', 'B7'],
             getScroll: 1,
+            speed: 1,
             scollX: '0px',
-            getScrollSmall: 1,
-            scrollXSmall: '0px'
         }
     },
     methods: {
@@ -19,13 +18,10 @@ export default {
                 if (this.getScroll > 6000 * 2) {
                     this.getScroll = 0
                 }
-                this.getScroll = this.getScroll + 5;
-                this.getScrollSmall = this.getScroll + 1;
-
+                this.getScroll = this.getScroll + this.speed;
                 // console.log(this.getScroll)
-                this.scollX = '-' + this.getScroll + 'px';
-                this.scrollXSmall = '-' + this.getScroll + 'px'
 
+                this.scollX = '-' + this.getScroll + 'px';
                 // console.log(this.scollX)
             }, 1);
         },
@@ -43,17 +39,11 @@ export default {
                 <h2 class="section-title">They talk about us</h2>
             </div>
         </div>
-        <div class="scroll-news d-none d-md-block">
+        <div class="scroll-news ">
             <div class="scroll d-flex">
                 <img v-for="newsImg in newsList" :src="store.getNewsImgPath(newsImg, 'png')" alt="News Logo">
             </div>
         </div>
-        <div class="scroll-news d-block d-md-none">
-            <div class="scroll-small d-flex">
-                <img v-for="newsImg in newsList" :src="store.getNewsImgPath(newsImg, 'png')" alt="News Logo">
-            </div>
-        </div>
-
     </section>
 </template>
 
@@ -65,15 +55,9 @@ section#talk-about-us {
 div.scroll-news {
     overflow: hidden;
 
-
     div.scroll {
         padding-left: 100%;
         transform: translateX(v-bind(scollX));
-    }
-
-    div.scroll-small {
-        padding-left: 100%;
-        transform: translateX(v-bind(scrollXSmall));
     }
 
     img {
@@ -82,7 +66,5 @@ div.scroll-news {
         object-fit: contain;
         margin-left: 100px;
     }
-
-
 }
 </style>
